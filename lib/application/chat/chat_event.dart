cb221136723b6cd6e23fdb2b6f2c33e8f63869fb
Jasim Ameen <89957373+jasimameenek@users.noreply.github.com>
2022-06-10 20:46:04 +0530

    clen code

--- lib/application/chat/chat_event.dart
+++ lib/application/chat/chat_event.dart
@@ -4,7 +4,6 @@ part of 'chat_bloc.dart';
 class ChatEvent with _$ChatEvent {
   const factory ChatEvent.navigateToChat(BuildContext context, int id) =
       _NavigateToChat;
-  const factory ChatEvent.started(int id) = _Started;
   const factory ChatEvent.sendMessage(int id, String message) = _SendMessage;
   const factory ChatEvent.receiveMessage() = _receiveMessage;
   const factory ChatEvent.typing(bool isTyping) = _Typing;
